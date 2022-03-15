6a46297a1866a8fb61efc35d36b7f6660a36f919
korchinmv <korchinmv@yandex.ru>
2022-03-15 10:56:09 +0300

    added design block

--- app/js/main.js
+++ app/js/main.js
@@ -1,11 +1,20 @@
 $(function () {
 
-  $('.main-slider__wrapper').slick({
-    dots: true,
-    arrows: false,
+	$('.main-slider__wrapper').slick({
+		dots: true,
+		arrows: false,
 
-  });
+	});
 
+	var containerEl1 = document.querySelector('[data-ref="container-1"]');  //микситап на 2 контейнера на странице
+	var containerEl2 = document.querySelector('[data-ref="container-2"]');
 
-  var mixer = mixitup('.product-week__wrapper');
+	var config = {
+		controls: {
+			scope: 'local'
+		}
+	};
+
+	var mixer1 = mixitup(containerEl1, config);
+	var mixer2 = mixitup(containerEl2, config);
 });
